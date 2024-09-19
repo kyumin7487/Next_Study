af1499e96dd3d58ecc16fd088d626c4fe9632a73
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-09-19 13:46:19 +0900

    feat :: 라우팅 겸 tailwindCSS

--- levelup-react-with-nextjs/app/layout.tsx
+++ levelup-react-with-nextjs/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import { GlobalNav } from '@/ui/global-nav'
+import '@/style/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
