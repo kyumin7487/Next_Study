2b4269452be8d06e835edfff54396cd3e11026bc
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-09-24 14:56:18 +0900

    feat :: 동적라우팅

---
+++ levelup-react-with-nextjs/app/dynamic-routing/layout.tsx
@@ -0,0 +1,42 @@
+import { getTopCategories } from '@/data/category';
+import { ClickCounter } from '@/ui/click-counter';
+import { TabGroup } from '@/ui/tab-group';
+import React from 'react';
+import { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+    title: '레벨업 Next.js: 동적 라우팅',
+}
+export default async function Layout({
+                                         children,
+                                     }: {
+    children: React.ReactNode;
+}) {
+    const topCategories = getTopCategories();
+
+    return (
+        <div className="space-y-9">
+            <div className="flex justify-between">
+                <TabGroup
+                    path="/dynamic-routing"
+                    items={[
+                        {
+                            text: '홈',
+                        },
+                        ...topCategories.map((x) => ({
+                            text: x.name,
+                            slug: x.slug,
+                        })),
+                    ]}
+                />
+
+                <div className="self-start">
+                    <ClickCounter />
+                </div>
+            </div>
+
+            <div>{children}</div>
+        </div>
+    );
+}
