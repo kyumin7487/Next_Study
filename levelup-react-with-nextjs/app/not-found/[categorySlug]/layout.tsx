953d642c9d1ac40c320ea9c68a9830e433d1f778
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-09-26 13:57:20 +0900

    feat :: not-found categorySlug

---
+++ levelup-react-with-nextjs/app/not-found/[categorySlug]/layout.tsx
@@ -0,0 +1,45 @@
+import { getCategories, getCategory } from '@/data/category';
+import { ClickCounter } from '@/ui/click-counter';
+import { TabGroup } from '@/ui/tab-group';
+
+export default async function Layout({
+    children,
+    params,
+                                     }: {
+    children: React.ReactNode;
+    params: { categorySlug: string };
+}) {
+    const category = getCategory(params.categorySlug);
+    const items = getCategories(params.categorySlug);
+
+    return (
+        <div className="space-y-9">
+            <div>
+                <div className="flex justify-between">
+                    <TabGroup
+                        path={`/not-found/${category.slug}`}
+                        items={[
+                            {
+                                text: '전체',
+                            },
+                            ...items.map((x) => ({
+                                text: x.name,
+                                slug: x.slug,
+                            })),
+                            {
+                                text: '❗미존재 하위항목❗',
+                                slug: 'does-not-exist',
+                            },
+                        ]}
+                    />
+
+                    <div className="self-start">
+                        <ClickCounter />
+                    </div>
+                </div>
+            </div>
+
+            <div>{children}</div>
+        </div>
+    );
+}
